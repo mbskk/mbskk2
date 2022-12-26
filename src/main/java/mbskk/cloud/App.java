c98ee6e95f67083cae61b0344cb65778547ebcec
root <root@mbskk.localdomain>
2022-12-26 22:26:15 +0530

    All files  committed at a time

---
+++ src/main/java/mbskk/cloud/App.java
@@ -0,0 +1,17 @@
+package mbskk.cloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( " " );
+        System.out.println( "Hello World!" );
+        System.out.println( "This is Sai Kiran Krishna" );
+        System.out.println( "Learning Maven" );
+        System.out.println( " " );
+    }
+}
