9859c89c9587b8e93ccd78f26472efae020458ff
mbskk <101353849+mbskk@users.noreply.github.com>
2022-12-27 00:38:18 +0530

    Update App.java

--- src/main/java/mbskk/cloud/App.java
+++ src/main/java/mbskk/cloud/App.java
@@ -13,6 +13,6 @@ public class App
         System.out.println( "This is Sai Kiran Krishna" );
         System.out.println( "Learning Maven" );
         System.out.println( "Don't quit " );
-         System.out.println( " " );
+         System.out.println( "Never give up " );
     }
 }
